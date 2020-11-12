8f168fe1be621eca9f798c7c7ba679563475d964
Maxim Konstantinov <konstantinovmax@gmail.com>
2020-11-12 20:31:02 +0300

    feat: изменить стили у страниц

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import toolImage from '../images/gear2.png';
 
 function AboutMe() {
   return (
     <div className="section">
         <h1 className="section__title">ВЕБ-РАЗРАБОТЧИК</h1>
+        <h2 className="section__title">ВЕБ-РАЗРАБОТЧИК</h2>
+        <h3 className="section__title">ВЕБ-РАЗРАБОТЧИК</h3>
+        <h4 className="section__title">ВЕБ-РАЗРАБОТЧИК</h4>
+        <h5 className="section__title">ВЕБ-РАЗРАБОТЧИК</h5>
+        <h6 className="section__title">ВЕБ-РАЗРАБОТЧИК</h6>
+        <p className="section__title">ВЕБ-РАЗРАБОТЧИК</p>
+        <img className="section__tool-image rotation" src={ toolImage } />
     </div>
   );
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import toolImage from '../images/gear2.png';
 
 function Portfolio() {
   return (
     <div className="section">
         <h1 className="section__title">СПИСОК ПРОЕКТОВ</h1>
+        <h2 className="section__title">СПИСОК ПРОЕКТОВ</h2>
+        <h3 className="section__title">СПИСОК ПРОЕКТОВ</h3>
+        <h4 className="section__title">СПИСОК ПРОЕКТОВ</h4>
+        <h5 className="section__title">СПИСОК ПРОЕКТОВ</h5>
+        <h6 className="section__title">СПИСОК ПРОЕКТОВ</h6>
+        <p className="section__title">СПИСОК ПРОЕКТОВ</p>
+        <img className="section__tool-image rotation" src={ toolImage } />
     </div>
   );
 }
