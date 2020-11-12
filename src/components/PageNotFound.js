02ce17f9683cb77ed66d23a42c8502ffcb8189ca
Maxim Konstantinov <konstantinovmax@gmail.com>
2020-11-12 20:21:11 +0300

    feat: применение стилей к странице 404

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function PageNotFound () {
   return (
     <div className="section">
-      <h1 className="section__title">404 - Страница не найдена</h1>
-      <Link className="button button_type_to-main" to="/">Назад</Link>
+      <h1 className="section__title">404 - СТРАНИЦА НЕ НАЙДЕНА</h1>
+      <Link className="button button_type_to-main" to="/">НАЗАД</Link>
     </div>
   )
 }
